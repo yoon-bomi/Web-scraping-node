fda71baa10dcfe56e12cd990e1be002f639b4ca5
Windy <ydg0330@naver.com>
2022-05-07 12:24:33 +0900

    feat : create musicSummary route

---
+++ src/routes/music-chart/summary/get.ts
@@ -0,0 +1,41 @@
+import { Spec } from "koa-joi-router";
+import * as Joi from "@hapi/joi";
+import { MusicChartService } from "services/musicChart/application/service";
+
+const paramsSchema = Joi.object({
+  vendor: Joi.string().valid("melon", "genie", "vibe"),
+});
+
+const outputSchema = Joi.array()
+  .items(
+    Joi.object({
+      ranking: Joi.number().required(),
+      name: Joi.string().required(),
+      singer: Joi.string().required(),
+      timezone: Joi.string().required(),
+      album: Joi.string().required(),
+    })
+  )
+  .required();
+
+export default {
+  path: "/:vendor/summary",
+  method: "get",
+  validate: {
+    params: paramsSchema,
+    output: {
+      200: {
+        body: outputSchema,
+      },
+    },
+  },
+  handler: async (ctx) => {
+    const { vendor } = ctx.request.params;
+
+    const musicChartService = new MusicChartService();
+
+    const musicDetailList = await musicChartService.getList(vendor);
+
+    ctx.body = musicDetailList;
+  },
+} as Spec;
