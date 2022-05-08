2a8a9a4426dee9abe9f3faff0b83027befdcf6e2
Windy <ydg0330@naver.com>
2022-05-08 16:34:53 +0900

    refactor : MusicChartService

--- src/services/musicChart/application/service.ts
+++ src/services/musicChart/application/service.ts
@@ -1,17 +1,65 @@
-import { Inject } from "typedi";
+import { badRequest, lengthRequired } from "@hapi/boom";
 import { MusicChartRepository } from "../infrastructure/repository";
 
 export class MusicChartService {
-  @Inject()
-  private musicChartRepository!: MusicChartRepository;
-
   async getAllList(vendor: string) {
-    console.log("test ok");
+    const musicChartRepository = new MusicChartRepository();
+    const musicInfos = await musicChartRepository.getMusicInfos(vendor);
+
+    if (musicInfos.length === 0) {
+      throw lengthRequired(`data is empty in ${vendor}`);
+    }
+
+    return musicInfos.map((info) => {
+      return {
+        id: info.id,
+        ranking: info.musicSummary.ranking,
+        name: info.musicSummary.name,
+        singer: info.musicSummary.singer,
+        album: info.musicSummary.album,
+        publisher: info.musicDetail.publisher,
+        agency: info.musicDetail.agency,
+      };
+    });
   }
 
-  async getList(vendor: string) {}
+  async getList(vendor: string) {
+    const musicChartRepository = new MusicChartRepository();
+    const musicInfos = await musicChartRepository.getMusicInfos(vendor);
+
+    if (musicInfos.length === 0) {
+      throw lengthRequired(`data is empty in ${vendor}`);
+    }
+
+    return musicInfos.map((info) => {
+      return {
+        id: info.id,
+        ranking: info.musicSummary.ranking,
+        name: info.musicSummary.name,
+        singer: info.musicSummary.singer,
+        album: info.musicSummary.album,
+      };
+    });
+  }
 
   async getMusicDetail(vendor: string, musicId: number) {
-    console.log("test ok");
+    const musicChartRepository = new MusicChartRepository();
+    const musicInfos = await musicChartRepository.getMusicInfos(vendor);
+
+    const [music] = musicInfos.filter((info) => info.id === musicId);
+
+    if (music === undefined) {
+      throw badRequest(`not found (${musicId})`);
+    }
+
+    return {
+      id: music.id,
+      ranking: music.musicSummary.ranking,
+      name: music.musicSummary.name,
+      singer: music.musicSummary.singer,
+      album: music.musicSummary.album,
+      publisher: music.musicDetail.publisher,
+      agency: music.musicDetail.agency,
+    };
   }
 }
